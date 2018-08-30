ddd464548298169b1d56f25136cacce17e70175f
Aline Abler <alinea@riseup.net>
2018-08-30 14:00:55 +0200

    Make the dark magic part more legible

--- mentions_handler.py
+++ mentions_handler.py
@@ -131,9 +131,11 @@ class MentionsHandler(Handler):
                     for entity in message.entities:
                         if entity.type == "mention":
                             unicode_text = message.text.encode('utf-16')
-                            # Multiply all offsets by 2 and add 2 (4 in the first case because we want to get rid of the @)
-                            # I don't know, okay? I just don't know.
-                            mentioned = unicode_text[entity.offset*2  + 4:entity.offset*2 + entity.length*2 + 2]
+                            # Multiply all offsets by 2 because we're still counting 8-bit units although
+                            # the string is utf-16.
+                            # Add 2 to first offset - one to consume the BOM, one to consume the @ character of the mention
+                            # Add only 1 to the last offset, for the BOM
+                            mentioned = unicode_text[(entity.offset + 2) * 2:(entity.offset + entity.length + 1) * 2]
                             if mentioned.decode('utf-16') == self.name:
                                 res = True
 
